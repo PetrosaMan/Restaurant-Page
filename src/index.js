28c81671c81977c5e49b3f2fd6f4111bcf0cf82c
petrosaMan <dw.todd.eng@gmail.com>
2025-11-24 10:15:20 +0200

    Completed styling of home page

--- src/index.js
+++ src/index.js
@@ -10,29 +10,29 @@ const tab_1 = document.querySelector('.tab-1');
 const tab_2 = document.querySelector('.tab-2');
 const tab_3 = document.querySelector('.tab-3');
 
-tab_1.style.backgroundColor = 'red';
-tab_2.style.backgroundColor = 'green';
-tab_3.style.backgroundColor = 'green';
+tab_1.style.backgroundColor = '#6ed86e';
+tab_2.style.backgroundColor = '#ccffcc';
+tab_3.style.backgroundColor = '#ccffcc';
 
 tab_1.addEventListener('click', () => {
     console.log('tab-1 clicked');
-    tab_1.style.backgroundColor = 'red';
-    tab_2.style.backgroundColor = 'green';
-    tab_3.style.backgroundColor = 'green';
+    tab_1.style.backgroundColor = '#6ed86e';
+    tab_2.style.backgroundColor = '#ccffcc';
+    tab_3.style.backgroundColor = '#ccffcc';
     renderHome();
 });
 
 tab_2.addEventListener('click', () => {
     console.log('tab-2 clicked');
-    tab_2.style.backgroundColor = 'red';
-    tab_1.style.backgroundColor = 'green';
-    tab_3.style.backgroundColor = 'green';
+    tab_2.style.backgroundColor = '#6ed86e';
+    tab_1.style.backgroundColor = '#ccffcc';
+    tab_3.style.backgroundColor = '#ccffcc';
 });
 
 tab_3.addEventListener('click', () => {
     console.log('tab-3 clicked');
-    tab_3.style.backgroundColor = 'red';
-    tab_1.style.backgroundColor = 'green';
-    tab_2.style.backgroundColor = 'green';
+    tab_3.style.backgroundColor = '#6ed86e';
+    tab_1.style.backgroundColor = '#ccffcc';
+    tab_2.style.backgroundColor = '#ccffcc';
 });
 
