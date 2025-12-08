5ed4fc7d1034d3a106cc0544e29581fc85dc9e9f
PetrosaMan <dw.todd.eng@gmail.com>
2025-12-08 15:36:41 +0200

    Code changes to create a production mode.

--- src/index.js
+++ src/index.js
@@ -3,40 +3,43 @@
 import { renderHome } from "./home-module.js";
 import { renderMenu } from "./menu-module.js";
 import { renderAbout } from "./about-module.js";
-import './style.css';
-import kafeImg from './assets/kafeSerefe.png';
+import "./style.css";
+import kafeImg from "./assets/kafeSerefe.png";
 
-const tab_1 = document.querySelector('.tab-1');
-const tab_2 = document.querySelector('.tab-2');
-const tab_3 = document.querySelector('.tab-3');
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looka like we are in development mode!");
+}
 
-tab_1.style.backgroundColor = '#ffffcc';
-tab_2.style.backgroundColor = '#ffffcc';
-tab_3.style.backgroundColor = '#ffffcc';
+const tab_1 = document.querySelector(".tab-1");
+const tab_2 = document.querySelector(".tab-2");
+const tab_3 = document.querySelector(".tab-3");
 
-tab_1.addEventListener('click', () => {
-    console.log('tab-1 clicked');
-    tab_1.style.backgroundColor = '#ffff99';
-    tab_2.style.backgroundColor = '#ffffcc';
-    tab_3.style.backgroundColor = '#ffffcc';
-    renderHome();
+tab_1.style.backgroundColor = "#ffffcc";
+tab_2.style.backgroundColor = "#ffffcc";
+tab_3.style.backgroundColor = "#ffffcc";
+
+tab_1.addEventListener("click", () => {
+  console.log("tab-1 clicked");
+  tab_1.style.backgroundColor = "#ffff99";
+  tab_2.style.backgroundColor = "#ffffcc";
+  tab_3.style.backgroundColor = "#ffffcc";
+  renderHome();
 });
 
-tab_2.addEventListener('click', () => {
-    console.log('tab-2 clicked');
-    tab_2.style.backgroundColor = '#ffff99';
-    tab_1.style.backgroundColor = '#ffffcc';
-    tab_3.style.backgroundColor = '#ffffcc';
-    renderMenu();
+tab_2.addEventListener("click", () => {
+  console.log("tab-2 clicked");
+  tab_2.style.backgroundColor = "#ffff99";
+  tab_1.style.backgroundColor = "#ffffcc";
+  tab_3.style.backgroundColor = "#ffffcc";
+  renderMenu();
 });
 
-tab_3.addEventListener('click', () => {
-    console.log('tab-3 clicked');
-    tab_3.style.backgroundColor = '#ffff99';
-    tab_1.style.backgroundColor = '#ffffcc';
-    tab_2.style.backgroundColor = '#ffffcc';
-    renderAbout();
+tab_3.addEventListener("click", () => {
+  console.log("tab-3 clicked");
+  tab_3.style.backgroundColor = "#ffff99";
+  tab_1.style.backgroundColor = "#ffffcc";
+  tab_2.style.backgroundColor = "#ffffcc";
+  renderAbout();
 });
 
 document.getElementById("defaultOpen").click();
-
---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+// webpack.common.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      title: "Production",
+    }),
+  ],
+  output: {
+    filename: "[name].main.js", // is [name] ok?
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  // loaders
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
